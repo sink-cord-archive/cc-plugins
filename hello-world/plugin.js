eea276983785282ad14e0ecc548950e169287306
Remty5 <Remty5@users.noreply.github.com>
2021-08-26 11:52:01 +0000

    deploy: d813dcfd15bd61f4e1c345d671e3fee95c789ff3

---
+++ hello-world/plugin.js
@@ -0,0 +1,1 @@
+(function(){"use strict";return{onLoad(){console.log("Hello world! :)")},onUnload(){console.log("Goodbye, world.. :(")}}})();
---
+++ spec!al ch@rs test/plugin.js
@@ -0,0 +1,1 @@
+(function(){"use strict";return{onLoad(){console.log("Special chars plugin loaded!")},onUnload(){console.log("Special chars plugin unloading")}}})();
