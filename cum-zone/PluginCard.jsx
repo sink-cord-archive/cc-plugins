dc12fa171fee5cf02d941ade5552bf96e5366cf1
Cain Atkinson <yellowsink@protonmail.com>
2021-10-18 21:19:13 +0100

    more user friendly

--- cum-zone/PluginCard.jsx
+++ cum-zone/PluginCard.jsx
@@ -1,5 +1,7 @@
 import { findByDisplayName, findByProps } from "@cumcord/modules/webpack";
 const FormText = findByDisplayName("FormText");
+import plugins from "@cumcord/plugins";
+import ui from "@cumcord/ui";
 
 // props taken from https://github.com/Cumcord/Cumcord/blob/stable/src/api/ui/settings/components/Plugins.jsx
 const Button = findByProps("Sizes", "Colors", "Looks", "DropdownSizes");
@@ -8,12 +10,19 @@ export default ({ plugin }) => (
     <div className="ysink_card">
         <div className="ysink_toprow">
             <FormText className="ysink_title">{plugin.name}</FormText>
-            <Button className="ysink_button"
+            <Button
+                className="ysink_button"
                 color={Button.Colors.BRAND}
                 size={Button.Sizes.SMALL}
                 look={Button.Looks.OUTLINED}
                 onClick={() => {
-                    cumcord.plugins.importPlugin("https://" + plugin.url);
+                    let promise = plugins.importPlugin("https://" + plugin.url);
+                    promise.then(() =>
+                        ui.toasts.showToast({
+                            title: "Installed plugin " + plugin.name,
+                            duration: 5000,
+                        })
+                    );
                 }}
             >
                 Install
