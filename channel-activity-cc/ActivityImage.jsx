ed20b4814047fca30ff6e0df32974eb25bdaacae
Cain Atkinson <yellowsink@protonmail.com>
2021-10-18 12:54:30 +0100

    ye

---
+++ channel-activity-cc/ActivityImage.jsx
@@ -0,0 +1,5 @@
+import { memo } from "react";
+
+export default memo(({ src }) =>
+    <img src={src} className="ysink_activity_image" />
+);
---
+++ channel-activity-cc/index.jsx
@@ -0,0 +1,58 @@
+import ActivityImage from "./ActivityImage";
+import CssInject from "./styles.css"; // cumcord go brr
+
+export default (data) => {
+    let memberListItemUnPatch = null;
+    let cssUnpatch = null;
+
+    return {
+        onLoad() {
+            cssUnpatch = CssInject();
+
+            let memberlistitem =
+                cumcord.modules.webpack.findByDisplayName(
+                    "MemberListItem"
+                ).prototype;
+            memberListItemUnPatch = cumcord.patcher.after(
+                "render",
+                memberlistitem,
+                // args are always empty here so ignore them
+                (_, retVal) => {
+                    // get activities
+                    if (!retVal.props?.subText?.props) return;
+                    const activities =
+                        retVal?.props?.subText?.props?.activities;
+                    if (!activities?.length) return;
+
+                    // remove exising children
+                    retVal.props.children = [];
+
+                    // add a child for each activity
+                    for (const activity of activities) {
+                        if (
+                            activity.application_id &&
+                            activity.assets &&
+                            (activity.assets?.large_image ||
+                                activity.assets?.small_image)
+                        ) {
+                            const activityImgUrl = `https://cdn.discordapp.com/app-assets/${
+                                activity.application_id
+                            }/${
+                                activity.assets?.large_image ||
+                                activity.assets?.small_image
+                            }.png`;
+                            retVal.props.children.push(
+                                <ActivityImage src={activityImgUrl} />
+                            );
+                        }
+                    }
+
+                    return retVal;
+                }
+            );
+        },
+        onUnload() {
+            if (memberListItemUnPatch) memberListItemUnPatch();
+        },
+    };
+};
