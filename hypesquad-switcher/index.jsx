c91df657d9d3d0b67c653d058f260e1ab3767e55
Cain Atkinson <yellowsink@protonmail.com>
2021-10-19 22:38:48 +0100

    yeah it done

---
+++ hypesquad-switcher/index.jsx
@@ -0,0 +1,57 @@
+import cssInject from "./styles.css";
+import { findByProps, findByDisplayName } from "@cumcord/modules/webpack";
+import { after } from "@cumcord/patcher";
+import { SwitchButtonArray, SwitchButton } from "./SwitchButtonArray.jsx";
+
+export default (data) => {
+    let patches = [];
+
+    return {
+        onLoad() {
+            patches.push(cssInject());
+
+            let currentHouseBanner =
+                findByDisplayName("JoinHypeSquadCTA").prototype;
+
+            patches.push(
+                after("render", currentHouseBanner, (_, retVal) => {
+                    console.log(retVal);
+                    let inSquadValid = retVal?.props?.children?.props?.children;
+                    let notInSquadValid =
+                        retVal?.props?.children?.[0]?.props?.children?.[1]
+                            ?.props?.children?.props?.children;
+
+                    if (inSquadValid) {
+                        retVal.props.children.props.children.push(
+                            <SwitchButtonArray />
+                        );
+                        return retVal;
+                    }
+
+                    if (notInSquadValid) {
+                        retVal.props.children[0].props.children[1].props.children =
+                            [
+                                retVal.props.children[0].props.children[1].props
+                                    .children,
+                            ];
+
+                        retVal.props.children[0].props.children[1].props.children[0].props.children =
+                            "Take HypeSquad test";
+
+                        retVal.props.children[0].props.children[1].props.children.push(
+                            <div className="ysink_hypesquad_container ysink_new">
+                                {[1, 2, 3].map((n) => (
+                                    <SwitchButton houseNum={n} />
+                                ))}
+                            </div>
+                        );
+
+                        return retVal;
+                    }
+                })
+            );
+        },
+
+        onUnload: () => patches.forEach((unpatch) => unpatch()),
+    };
+};
