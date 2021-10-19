87ae4a30e904e7f8d9ff043587e6cbefbaa0dba2
Cain Atkinson <yellowsink@protonmail.com>
2021-10-19 20:42:23 +0100

    fix repo modal

--- cum-zone/RepoCard.jsx
+++ cum-zone/RepoCard.jsx
@@ -5,39 +5,37 @@ const FormDivider = findByDisplayName("FormDivider");
 const Button = findByProps("Sizes", "Colors", "Looks", "DropdownSizes");
 const Badges = cumcord.modules.webpack.findByProps("BadgeShapes");
 
-export default ({ repo, nest }) => {
-    return (
-        <div className="ysink_card">
-            <div className="ysink_row">
-                <div>
-                    <FormTitle tag="p" className="ysink_title">
-                        {repo.name}
-                        {repo.official ? (
-                            <Badges.TextBadge
-                                className="ysink_badge"
-                                text="official repo"
-                                color="var(--info-positive-foreground)"
-                            />
-                        ) : (
-                            []
-                        )}
-                    </FormTitle>
+export default ({ repo, nest }) => (
+    <div className="ysink_card">
+        <div className="ysink_row">
+            <div>
+                <FormTitle tag="p" className="ysink_title">
+                    {repo.name}
+                    {repo.official ? (
+                        <Badges.TextBadge
+                            className="ysink_badge"
+                            text="official repo"
+                            color="var(--info-positive-foreground)"
+                        />
+                    ) : (
+                        []
+                    )}
+                </FormTitle>
 
-                    <FormText>{repo.url}</FormText>
-                </div>
-
-                <Button
-                    color={Button.Colors.RED}
-                    className="ysink_button"
-                    onClick={() =>
-                        (nest.store.repos = nest.ghost.repos.filter(
-                            (r) => r.url != repo.url
-                        ))
-                    }
-                >
-                    Remove Repo
-                </Button>
+                <FormText>{repo.url.href}</FormText>
             </div>
+
+            <Button
+                color={Button.Colors.RED}
+                className="ysink_button"
+                onClick={() =>
+                    (nest.store.repos = nest.ghost.repos.filter(
+                        (r) => r.url != repo.url
+                    ))
+                }
+            >
+                Remove Repo
+            </Button>
         </div>
-    );
-};
+    </div>
+);
