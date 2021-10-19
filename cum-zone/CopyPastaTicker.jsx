757585b86483884916cad8a100cd4b7441074333
Cain Atkinson <yellowsink@protonmail.com>
2021-10-19 15:31:01 +0100

    idk

--- cum-zone/CopyPastaTicker.jsx
+++ cum-zone/CopyPastaTicker.jsx
@@ -4,8 +4,22 @@ const FormText = findByDisplayName("FormText");
 const pasta =
     "Welcome to the cum zone - Only cum inside anime girls - Quivering clit, double jointed pussy - Fresh balls, elegant ejaculation - First the kiss, then the cum - My dick is in love with pain - Co-op cock torture - Stuff my dick into a furnace - Stitch my cock shut - Pressure cook my greasy balls - Cumblast me and make it snappy - Cum, cum, cum, cum, cum, cum, cum, cum - Cum, cum, cum, cum, cum, cum, cum, cum - Cum, cum, cum, cum, cum, cum, cum, cum - Cum, cum, cum, cum, cum, cum, cum, cum - What's all the cummotion? - My dad fell into a cum shaft - My dad glazed my face with cum - Fertilize a baby with hunk spunk - Cum spunk in my trunk - Cum craving toddler - Cum drippin' cunt - Cummy Rae Jepsen - Cum me maybe - Cummy bottom boy - Night of the living cum - Nefarious cum mastermind - Cum makes me fearless - Cum crammer, cock slammer - Cum slammed ya mum - Mail your mums pieces of my dick - Bazinga! - Chug the cum, fug ya mum - Fuck my asshole full of cum - Three little words - Get fucked, nerd - Cum stuffer, jenkem huffer - Fuck my cum puddle - Bottom stuffer, semen huffer - Would love a gator to fuck me - Undercooked baby pig penises - Help my dogs get a huge boner - Water bong full of cat cum - Accidentally fucked my own ass - I barely had any dicks inside me - Who ate all my cum? A mystery - Cum detective hot on the trail - Bees make honey, I make cummy";
 
-export default () => (
-    <FormText>
-        <marquee className="ysink_cuminside" scrolldelay="65">{pasta}</marquee>
-    </FormText>
-);
+let arr = [];
+for (let i = 0; i < 40; i++) arr.push(0);
+
+export default ({ fillscreen }) =>
+    fillscreen ? (
+        <FormText className="ysink_ticker_fillscreen">
+            {arr.map((_) => (
+                <marquee className="ysink_cuminside" scrolldelay="65">
+                    {pasta}
+                </marquee>
+            ))}
+        </FormText>
+    ) : (
+        <FormText>
+            <marquee className="ysink_cuminside" scrolldelay="65">
+                {pasta}
+            </marquee>
+        </FormText>
+    );
