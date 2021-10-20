6d6960449f09e772ba5c1669c8b6a6c54f2835d4
Cain Atkinson <yellowsink@protonmail.com>
2021-10-20 21:55:50 +0100

    panel should open on macs

--- command-palette/patches/keybind.js
+++ command-palette/patches/keybind.js
@@ -2,7 +2,7 @@ import openPalette from "../components/Palette.jsx";
 
 export default (nest, defaultEntries) => {
     let keyHandler = (e) => {
-        if (e.ctrlKey && e.shiftKey && e.which == 80) openPalette(nest, defaultEntries);
+        if ((e.ctrlKey || e.metaKey) && e.shiftKey && e.which == 80) openPalette(nest, defaultEntries);
     };
 
     document.addEventListener("keyup", keyHandler);
