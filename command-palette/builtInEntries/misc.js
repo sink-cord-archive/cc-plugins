fea8e092e12796713078fdd1a53b59fa84d5c149
Cain Atkinson <yellowsink@protonmail.com>
2021-10-28 19:58:55 +0100

    guild mute

--- command-palette/builtInEntries/misc.js
+++ command-palette/builtInEntries/misc.js
@@ -6,6 +6,8 @@ const { updateChannelOverrideSettings } = findByProps(
 );
 const { getCurrentChannelSettings } = findByProps("getCurrentChannelSettings");
 const { getChannelId } = findByProps("getChannelId", "getVoiceChannelId");
+const { updateGuildNotificationSettings } = findByProps("updateGuildNotificationSettings");
+const { isMuted } = findByProps("getMuteConfig")
 
 const source = "Built In";
 
@@ -19,6 +21,20 @@ export default [
             let muted = getCurrentChannelSettings(getGuildId(), getChannelId()).channel_is_muted;
             // i don't know if these last two fields are actually necessary or not, but just to be safe :)
             updateChannelOverrideSettings(getGuildId(), getChannelId(), {
+                muted: !muted,
+                suppress_everyone: !muted,
+                suppress_roles: !muted,
+            });
+        },
+    },
+    {
+        source,
+        icon: "🔇",
+        id: "misc_toggle_guild_mute",
+        label: "Toggle current guild mute",
+        action: () => {
+            let muted = isMuted(getGuildId());
+            updateGuildNotificationSettings(getGuildId(), {
                 muted: !muted,
                 suppress_everyone: !muted,
                 suppress_roles: !muted,
