a003c3a552156bdd99a184b3379f27ad8a21961e
e-boi <hiboysismyoufake6789@gmail.com>
2021-11-06 17:32:22 -0400

    fix and be able to copy url

--- create-emote/patchEmotePicker.jsx
+++ create-emote/patchEmotePicker.jsx
@@ -1,44 +1,27 @@
-import { findByProps, find } from "@cumcord/modules/webpack";
+import { find } from "@cumcord/modules/webpack";
 import { after } from "@cumcord/patcher";
 import ContextMenuInjection from "./ContextMenuInjection.jsx";
 
-const ContextMenu = findByProps("MenuGroup", "default");
-const contextMenuTools = findByProps("openContextMenu");
-
 export default () => {
-    const emojiPickerListRow = find(
-        (m) => m?.default?.displayName == "EmojiPickerListRow"
+    const EmojiContextMenu = find(
+        (m) => m.default?.displayName === "ExpressionPickerContextMenu"
     );
 
-    return after("default", emojiPickerListRow, (args, retVal) => {
-        if (!Array.isArray(retVal?.props?.children)) return retVal;
+    return after("default", EmojiContextMenu, ([{ target }], retVal) => {
+        if (!target.firstChild.currentSrc) return retVal;
+        if (!Array.isArray(retVal.props.children.props.children))
+            retVal.props.children.props.children = [
+                retVal.props.children.props.children,
+            ];
 
-        for (const emoji of retVal.props.children) {
-            if (!emoji || !emoji?.props?.children) continue;
+        retVal.props.children.props.children.push(
+            ContextMenuInjection({
+                isEmote: true,
+                emoteAlt: `:${target.dataset.name}:`,
+                url: target.firstChild.currentSrc,
+            })
+        );
 
-            let selectedEmoji = emoji.props.children.props.emoji;
-            emoji.props.onContextMenu = (e) => {
-                let selectedEmojiUrl = selectedEmoji.managed
-                    ? `https://discord.com${selectedEmoji.url}`
-                    : selectedEmoji.url;
-
-                contextMenuTools.openContextMenu(e, () => (
-                    <ContextMenu.default
-                        onClose={contextMenuTools.closeContextMenu}
-                    >
-                        {/*
-                            due to discord throwing errors if I just pass this as a component,
-                            i'll call it manually,
-                            as the root of this component is the allowed ContextMenu.MenuItem
-                         */}
-                        {ContextMenuInjection({
-                            isEmote: true,
-                            emoteAlt: `:${selectedEmoji.name}:`,
-                            url: selectedEmojiUrl,
-                        })}
-                    </ContextMenu.default>
-                ));
-            };
-        }
+        return retVal;
     });
 };
