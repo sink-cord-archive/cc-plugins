d7f7c13dc40301fac59892811ac3543412d14a66
Cain Atkinson <yellowsink@protonmail.com>
2021-11-19 21:23:35 +0000

    idk more stuff ig

--- cumstain/components/SettingsMain.jsx
+++ cumstain/components/SettingsMain.jsx
@@ -1,3 +1,27 @@
-export default () => (
-    <div style={{backgroundColor: "red"}}>Test</div>
-)
+import { findByDisplayName, findByProps } from "@cumcord/modules/webpack";
+import { persist } from "@cumcord/pluginData";
+import { useNest } from "@cumcord/utils";
+
+import { ErrorBoundary } from "@cumcord/ui/components";
+const FormTitle = findByDisplayName("FormTitle");
+const FormText = findByDisplayName("FormText");
+const FormSection = findByDisplayName("FormSection");
+const FormDivider = findByDisplayName("FormDivider");
+const Button = findByProps("Sizes", "Colors", "Looks", "DropdownSizes");
+const TextInput = findByDisplayName("TextInput");
+
+export default () => {
+    useNest(persist);
+
+    return (
+        <ErrorBoundary>
+            <FormSection>
+                <FormTitle tag="h1">Cumstain</FormTitle>
+
+                {(persist.ghost.themes ?? []).map((theme) => (
+                    <FormText>{theme.name}</FormText>
+                ))}
+            </FormSection>
+        </ErrorBoundary>
+    );
+};
