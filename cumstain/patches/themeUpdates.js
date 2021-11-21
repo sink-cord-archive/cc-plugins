9a60c9b7785e3671ab219f4e739fd5a37d09f20e
Cain Atkinson <yellowsink@protonmail.com>
2021-11-21 21:41:05 +0000

    new updater code

--- cumstain/patches/themeUpdates.js
+++ cumstain/patches/themeUpdates.js
@@ -1,6 +1,7 @@
-import { persist } from "@cumcord/pluginData";
+import { persist, state } from "@cumcord/pluginData";
 import { log } from "@cumcord/utils/logger";
 import fetchRepo from "../fetchRepo";
+import fetchTheme from "../fetchTheme";
 import {
     loadTheme,
     reloadTheme,
@@ -12,9 +13,29 @@ const checkAndApplyUpdates = async () => {
     log("|| CUMSTAIN || Started periodic theme update check");
 
     const repos = await Promise.all(persist.ghost.repos.map(fetchRepo));
-    const themes = repos.flatMap((repo) => repo.themes);
+    const repoThemes = repos.flatMap((repo) => repo.themes);
 
-    for (const theme of themes) {
+    for (let i = 0; i < persist.ghost.themes.length; i++) {
+        const theme = persist.ghost.themes[i];
+
+        const themeInRepo = repoThemes.find((t) => t.id === theme.id);
+        if (themeInRepo) {
+            persist.ghost.themes[i] = themeInRepo;
+        } else {
+            const fetchedTheme = await fetchTheme(theme.url).catch(() => {});
+            if (fetchedTheme) persist.ghost.themes[i] = fetchedTheme;
+        }
+
+        persist.store.themes[i].enabled = theme.enabled;
+        if (theme.enabled)
+            state.ghost.unpatchCache.get(theme.id)?.(
+                persist.ghost.themes[i].CSS
+            );
+
+        log(`|| CUMSTAIN || Updated theme ${persist.ghost.themes[i].name}`);
+    }
+
+    /* for (const theme of repoThemes) {
         const themeInCache = persist.ghost.themes.find(
             (t) => t.id === theme.id
         );
@@ -22,7 +43,7 @@ const checkAndApplyUpdates = async () => {
 
         reloadTheme(theme);
         log(`|| CUMSTAIN || Updated theme ${theme.name}`);
-    }
+    } */
 
     log("|| CUMSTAIN || Completed periodic theme update check");
 };
