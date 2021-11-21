2e2d5db1f367a985b0c120c721132541fdd6527c
Cain Atkinson <yellowsink@protonmail.com>
2021-11-21 12:15:24 +0000

    fix weirdness from not using repos

--- cumstain/components/TabInstalled.jsx
+++ cumstain/components/TabInstalled.jsx
@@ -1,37 +1,25 @@
 import { persist } from "@cumcord/pluginData";
 import { useNest } from "@cumcord/utils";
 
-const { useState, useEffect } = React;
+const { useState, useReducer } = React;
 
 import { ErrorBoundary } from "@cumcord/ui/components";
 import ThemeCard from "./ThemeCard";
-import fetchRepo from "../fetchRepo";
 import InstallBar from "./InstallBar";
-
-const getRepos = () => Promise.all(persist.ghost.repos.map(fetchRepo));
-
-const getThemes = async () =>
-    (await getRepos())
-        .flatMap((r) => r.themes)
-        .filter((t1) => persist.ghost.themes.some((t2) => t1.id === t2.id));
 
 export default () => {
     useNest(persist /* , false, (type, path) => path?.[0] === "themes" */);
 
-    let [themes, setThemes] = useState(undefined);
-    useEffect(() => {
-        if (!themes) getThemes().then(setThemes);
-    });
+    // React really doesn't like re-rendering when themes are deleted, so here we are
+    const [, rerender] = useReducer((x) => ~x, 0);
 
     return (
         <ErrorBoundary>
             <InstallBar />
+
             <div className="ysink_stain_cardcontainer">
-                {(themes ?? []).map((theme) => (
-                    <ThemeCard
-                        theme={theme}
-                        deleteHook={() => setThemes(undefined)}
-                    />
+                {persist.ghost.themes.map((theme) => (
+                    <ThemeCard theme={theme} deleteHook={rerender} />
                 ))}
             </div>
         </ErrorBoundary>
--- cumstain/fetchTheme.js
+++ cumstain/fetchTheme.js
@@ -4,7 +4,7 @@ export default async (url, repoUrl) => {
     const CSS = await (await fetch(new URL(url, repoUrl).href)).text();
 
     return {
-        id: `${repoUrl ?? "NOREPO"}___${url}`,
+        id: url,
         CSS,
         ...extractMeta(CSS),
     };
