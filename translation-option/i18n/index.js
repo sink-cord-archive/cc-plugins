f7c58c967f3d6c3d26c9a6fde6d3bc5940313934
Cain Atkinson <yellowsink@protonmail.com>
2021-12-19 22:05:00 +0000

    i18n i think?

--- translation-option/i18n/index.js
+++ translation-option/i18n/index.js
@@ -1,8 +1,19 @@
-const { readdirSync } = require('fs');
+import en_US from "./en-US.json";
+import ru from "./ru.json";
 
-readdirSync(__dirname)
-  .filter((file) => file !== 'index.js')
-  .forEach((filename) => {
-    const [ moduleName ] = filename.split('.');
-    module.exports[moduleName] = require(`${__dirname}/${filename}`);
-  });
+import { Messages, _chosenLocale } from "@cumcord/modules/common/i18n";
+
+export default () => {
+    if (_chosenLocale === "ru") {
+        Object.assign(Messages, ru);
+    } else {
+        Object.assign(Messages, en_US);
+    }
+
+    return () => {
+        for (const key in en_US) {
+            Messages[key] = undefined;
+            delete Messages[key];
+        }
+    };
+};
--- translation-option/index.js
+++ translation-option/index.js
@@ -1,27 +1,103 @@
-import React from "@cumcord/modules/common/React";
-import { find, findByProps } from "@cumcord/modules/webpack";
-const { getChannelId } = findByProps("getChannelId")
-import { Messages } from "@cumcord/modules/common/i18n";
+// this plugin is a port from pc, this file being based on https://github.com/powerfart-plugins/translation-option/blob/master/index.js
 
+// cumcord imports
+import { find, findByProps } from "@cumcord/modules/webpack";
+import { Messages } from "@cumcord/modules/common/i18n";
+import React from "@cumcord/modules/common/React";
+
+// react components
 import SettingsModal from './components/SettingsModal.jsx';
 import Settings from './components/Settings.jsx';
 
+// discord modules
+const { getChannelId } = findByProps("getChannelId")
+const Dispatcher = findByProps("_subscriptions");
+const { openModal } = findByProps("openModalLazy");
+
+// styles
+import styles from "./style.scss"
+
+// misc imports
 import  { settings } from './structures';
 import i18n from './i18n';
-
 import Translator from './engines';
 import { Patcher, OutputManager } from './utils';
 
-const Dispatcher = findByProps("_subscriptions");
+export default ({ persist, id }) => {
+    // some ported constructor & injection stuff
+    let outputManager = new OutputManager("translation-option", {});
+    let translator = new Translator(outputManager);
 
-export default () => {
+    // ported class members
+    const openSettingsModal = () => {
+        openModal((e) =>
+            React.createElement(SettingsModal, { Translator: translator })
+        );
+    };
 
+    const translate = (message, force) => {
+        if (!force && translator.isTranslated(message)) {
+          this.Translator.recover(message);
+          return;
+        }
+        translator.translateMessage(message, {
+          engine: this.settings.get('inEngine', null),
+          to: this.settings.get('inTo', null),
+          from: this.settings.get('inFrom', null)
+        })
+          .catch((err) => {
+            if (err.code === 'NO_TRANSLATION_ENGINE') {
+              outputManager.error(Messages.TRANSLATION_OPTION_CHOOSE_ENGINE, [ {
+                text: Messages.TRANSLATION_OPTION_SETTINGS,
+                onClick: openSettingsModal
+              } ]);
+              return;
+            }
+            outputManager.error(`${Messages.TRANSLATION_OPTION_ERRORS_TRANSLATE}: ${err.name}`);
+            this.error(
+              `${this.settings.get('inEngine', null)}:`,
+              `${this.settings.get('inFrom', 'auto')}->${this.settings.get('inTo', 'user lang')} \n`,
+              err
+            );
+          });
+      }
+
+    // remaining ported constructor & injection stuff
+    let patcher = new Patcher(translator, outputManager, {
+        openSettingsModal
+    });
+
+    patcher.inject();
+
+    Dispatcher.subscribe("MESSAGE_UPDATE" /* this.messageUpdate */);
+    Dispatcher.subscribe("MESSAGE_CREATE" /* this.messageCreate */);
+
+    let uninjectStyles = styles();
+
+    // janky, but adds i18n entries depending on the locale at plugin load time
+    let removei18n = i18n();
 
     return {
-        onUnload() {},
-        settings: React.createElement(SettingsModal, { Translator: this.Translator })
-    }
-}
+        onUnload() {
+            uninjectStyles();
+            removei18n();
+            
+            [...translator.messagesStorage.keys()].forEach((key) => {
+                const [channel_id, id] = key.split("-");
+                translator.recover({ channel_id, id });
+            });
+            translator.messagesStorage.clear();
+
+            patcher.uninject();
+
+            Dispatcher.unsubscribe("MESSAGE_UPDATE" /* this.messageUpdate */);
+            Dispatcher.unsubscribe("MESSAGE_CREATE" /* this.messageCreate */);
+        },
+        settings: React.createElement(SettingsModal, {
+            Translator: translator,
+        }),
+    };
+};
 
 /* module.exports =  */class TranslationOption extends Plugin {
   constructor () {
