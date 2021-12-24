43ff8633fbf0c0a7cc9662dcd91155ed85d1491d
Cain Atkinson <yellowsink@protonmail.com>
2021-12-24 16:20:00 +0000

    changes to codeblock, settings

--- codeblocks-plus/Settings.jsx
+++ codeblocks-plus/Settings.jsx
@@ -1,12 +1,15 @@
 import { persist } from "@cumcord/pluginData";
-import { findByDisplayName } from "@cumcord/modules/webpack";
+import { findByDisplayName, findByProps } from "@cumcord/modules/webpack";
 import { useNest } from "@cumcord/utils";
 import includedThemes from "./themeProcessor";
 
 const { SingleSelect } = findByDisplayName("Select", false);
+const Header = findByProps("Sizes", "Tags");
+const TextInput = findByDisplayName("TextInput");
 
 export default () => {
     useNest(persist);
+    const [custom, setCustom] = React.useState(persist.ghost.custom);
 
     const includedThemeOptions = includedThemes.map(({ name, url }) => ({
         value: url,
@@ -18,11 +21,21 @@ export default () => {
 
     return (
         <>
+            <Header className="ysink_code_head">Select theme</Header>
             <SingleSelect
                 options={themeOptions}
-                value={persist.ghost.theme}
+                value={persist.ghost.theme || undefined}
                 onChange={(e) => (persist.store.theme = e)}
-                isDisabled={false}
+                isDisabled={custom}
+            />
+
+            <Header className="ysink_code_head">Custom theme url</Header>
+            <TextInput
+                placeholder="Custom theme URL"
+                onChange={(e) =>
+                    setCustom((persist.store.theme = persist.store.custom = e))
+                }
+                value={custom}
             />
         </>
     );
--- codeblocks-plus/components/Codeblock.jsx
+++ codeblocks-plus/components/Codeblock.jsx
@@ -1,15 +1,48 @@
-export default ({ codeHtml, code, lang }) => (
-    <div className="ysink_code_wrapper hljs">
-        <div className="ysink_code_lang">{lang}</div>
-        <pre>
-            {codeHtml ? (
-                <code
-                    className={`hljs ${lang}`}
-                    dangerouslySetInnerHTML={{ __html: codeHtml }}
-                />
-            ) : (
-                <code className={`hljs ${lang}`}>{code}</code>
-            )}
-        </pre>
-    </div>
-);
+import { copyText } from "@cumcord/utils";
+const { useState, useEffect } = React;
+
+const flat = (html) =>
+    new DOMParser().parseFromString(html, "text/html").children[0].textContent;
+
+export default ({ codeHtml, code, lang }) => {
+    // >0: need to wait this amount next render
+    // 0: no cooldown
+    // -1: currently waiting for cooldown
+    let [cooldown, setCooldown] = useState(0);
+    useEffect(() => {
+        if (cooldown > 0) {
+            setCooldown(-1);
+            setTimeout(() => setCooldown(0), cooldown);
+        }
+    });
+
+    return (
+        <div className="ysink_code_wrapper hljs">
+            <div className="ysink_code_row">
+                <div className="ysink_code_lang">{lang}</div>
+                <button
+                    className="dark"
+                    disabled={cooldown}
+                    onClick={() => {
+                        if (cooldown) return;
+
+                        copyText(code ? code : flat(codeHtml));
+                        setCooldown(2000);
+                    }}
+                >
+                    {cooldown ? "Copied!" : "Copy"}
+                </button>
+            </div>
+            <pre>
+                {codeHtml ? (
+                    <code
+                        className={`hljs ${lang}`}
+                        dangerouslySetInnerHTML={{ __html: codeHtml }}
+                    />
+                ) : (
+                    <code className={`hljs ${lang}`}>{code}</code>
+                )}
+            </pre>
+        </div>
+    );
+};
