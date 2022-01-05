7dc1bf4cb29c59beec5e7d56d8110b9c4af02e0e
Cynthia Foxwell <gamers@riseup.net>
2022-01-04 20:38:44 -0700

    Channel Activity: fix media proxy pseudo-protocol assets

--- channel-activity/index.jsx
+++ channel-activity/index.jsx
@@ -28,20 +28,23 @@ export default (data) => {
 
                     // add a child for each activity
                     for (const activity of activities) {
-                        if (
-                            activity.application_id &&
-                            activity.assets &&
-                            (activity.assets?.large_image ||
-                                activity.assets?.small_image)
-                        ) {
-                            const activityImgUrl = `https://cdn.discordapp.com/app-assets/${
-                                activity.application_id
-                            }/${
-                                activity.assets?.large_image ||
-                                activity.assets?.small_image
-                            }.png`;
+                        const activityImage =
+                            activity?.assets?.large_image ||
+                            activity?.assets?.small_image;
+                        if (activity.application_id && activityImage) {
+                            const activityImgUrl = activityImage.startsWith(
+                                "mp:"
+                            )
+                                ? activityImage.replace(
+                                      "mp:",
+                                      "https://media.discordapp.net/"
+                                  )
+                                : `https://cdn.discordapp.com/app-assets/${activity.application_id}/${activityImage}.png`;
                             retVal.props.children.push(
-                                <img src={activityImgUrl} className="ysink_activity_image" />
+                                <img
+                                    src={activityImgUrl}
+                                    className="ysink_activity_image"
+                                />
                             );
                         }
                     }
