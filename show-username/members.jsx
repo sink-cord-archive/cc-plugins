b5d1117835b3f38a07f0c6371ca322725ee2091c
Cain Atkinson <yellowsink@protonmail.com>
2022-01-14 23:38:46 +0000

    thats much better

--- show-username/members.jsx
+++ show-username/members.jsx
@@ -4,16 +4,25 @@ import { after } from "@cumcord/patcher";
 
 export default () =>
     after(
-        "render",
-        findByDisplayName("MemberListItem").prototype,
+        "default",
+        findByDisplayName("ConnectedChannelMembers", false),
         (args, ret) => {
             if (persist.ghost.ml === false) return;
 
-            if (!ret?._owner?.pendingProps) return;
-            // ew
-            const pendProps = ret?._owner?.pendingProps;
-            if (pendProps.nick)
-                pendProps.nick = `${pendProps.nick} (${pendProps.user.username})`;
+            if (!ret?.props?.children?.props?.children?.props?.rows) return;
+            const rows = ret.props.children.props.children.props.rows;
+
+            for (let i = 0; i < rows.length; i++) {
+                if (rows[i]?.type !== "MEMBER" || rows[i].YSINK_USERN_PATCHED)
+                    continue;
+                // HAHA I CAN ATTACH STUFF TO THESE!!!!!!!
+                // annoyingly, these objects persist between renders
+                // so we need to patch them only once or
+                // nick (user) (user) (user) (user) etc.
+                rows[i].YSINK_USERN_PATCHED = true;
+                if (rows[i].nick)
+                    rows[i].nick = `${rows[i].nick} (${rows[i].user.username})`;
+            }
 
             return ret;
         }
