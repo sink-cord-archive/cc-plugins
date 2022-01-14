a7238a991ce0a4ac24245cc05535642a90d1bda6
Cain Atkinson <yellowsink@protonmail.com>
2022-01-14 23:41:14 +0000

    i hate this why

--- show-username/voice.js
+++ show-username/voice.js
@@ -6,10 +6,10 @@ export default () =>
     after("render", findByDisplayName("VoiceUsers").prototype, (args, ret) => {
         if (persist.ghost.vc === false) return;
 
-        if (!ret) return;
+        if (!ret?.props?.children) return;
         for (let i = 0; i < ret.props.children.length; i++) {
             const child = ret.props.children[0][i];
-            if (child.props.nick)
+            if (child?.props?.nick)
                 // const not consting moment
                 child.props.nick = `${child.props.nick} (${child.props.user.username})`;
         }
