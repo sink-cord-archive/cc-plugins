76e9dae0a7c7eb6cd245129961ebba524a43a132
Cain Atkinson <yellowsink@protonmail.com>
2022-01-27 20:25:51 +0000

    api stuff

--- devkitplus/api.js
+++ devkitplus/api.js
@@ -1,6 +1,7 @@
 import * as cctools from "cumcord-tools";
 
 import { findAll } from "@cumcord/modules/webpack";
+import { injectCSS } from "@cumcord/patcher";
 
 const findClassNameModuleAll = (className) => {
     if (className.startsWith(".")) className = className.substring(1);
@@ -13,11 +14,25 @@ const findClassNameModuleAll = (className) => {
     );
 };
 
+/* const compile = (scss, sass = false) =>
+    new Promise((resolve) => {
+        Sass.compile(scss, { indentedSyntax: sass }, (css) => resolve(css));
+    });
+
+const injectSCSS = async (scss, sass = false) => {
+    const modifyRaw = injectCSS(await compile(scss, sass));
+    return async (scss) =>
+        scss ? modifyRaw(await compile(scss, sass)) : modifyRaw();
+}; */
+
 export default () => {
     window.dk = {
         cctools,
         findClassNameModuleAll,
-        findClassNameModule: (className) => findClassNameModuleAll(className)[0],
+        findClassNameModule: (className) =>
+            findClassNameModuleAll(className)[0],
+        /* injectSCSS: (scss) => injectSCSS(scss),
+        injectSass: (sass) => injectSCSS(sass, true), */
     };
 
     return () => delete window.dk;
