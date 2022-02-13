1e9a729526ca4523a39601879b08cf77696b1d87
Cain Atkinson <yellowsink@riseup.net>
2022-02-13 17:08:55 +0000

    improve lazy patcher

--- create-emote/patchEmotePicker.jsx
+++ create-emote/patchEmotePicker.jsx
@@ -2,35 +2,27 @@ import { findByDisplayName } from "@cumcord/modules/webpack";
 import { after } from "@cumcord/patcher";
 import ContextMenuInjection from "./ContextMenuInjection.jsx";
 
-import lazyPatcher from "./lazyPatcher.js";
+import { lazyPatcher } from "cumcord-tools";
 
-export default () => {
-    lazyPatcher.lazyPatchContextMenu(
+export default () =>
+    lazyPatcher.patchContextMenu(
         "ExpressionPickerContextMenu",
-        (emojiContextMenu) => {
-            lazyPatcher.registerPatch(
-                after("default", emojiContextMenu, ([{ target }], retVal) => {
-                    if (!target.firstChild.currentSrc) return retVal;
-                    if (!Array.isArray(retVal.props.children.props.children))
-                        retVal.props.children.props.children = [
-                            retVal.props.children.props.children,
-                        ];
+        (emojiContextMenu) =>
+            after("default", emojiContextMenu, ([{ target }], retVal) => {
+                if (!target.firstChild.currentSrc) return retVal;
+                if (!Array.isArray(retVal.props.children.props.children))
+                    retVal.props.children.props.children = [
+                        retVal.props.children.props.children,
+                    ];
 
-                    retVal.props.children.props.children.push(
-                        ContextMenuInjection({
-                            isEmote: true,
-                            emoteAlt: `:${target.dataset.name}:`,
-                            url: target.firstChild.currentSrc,
-                        })
-                    );
+                retVal.props.children.props.children.push(
+                    ContextMenuInjection({
+                        isEmote: true,
+                        emoteAlt: `:${target.dataset.name}:`,
+                        url: target.firstChild.currentSrc,
+                    })
+                );
 
-                    return retVal;
-                })
-            );
-        }
+                return retVal;
+            })
     );
-
-    // both this and patchContextMenu export this,
-    // but a second call wont cause any issues.
-    return lazyPatcher.unpatchAll;
-};
