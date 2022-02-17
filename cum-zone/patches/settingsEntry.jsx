409e18ff40e623747bb11d67e13c91c92ed2fb5e
Cain Atkinson <yellowsink@riseup.net>
2022-02-17 10:31:52 +0000

    use findAsync

--- cum-zone/patches/settingsEntry.jsx
+++ cum-zone/patches/settingsEntry.jsx
@@ -1,26 +1,33 @@
-import { findByDisplayName } from "@cumcord/modules/webpack";
+import { findByDisplayName, findAsync } from "@cumcord/modules/webpack";
 import { after } from "@cumcord/patcher";
-import resetReposToDefault from "../defaultRepos.js";
-import { patchSettingsView } from "cumcord-tools";
+import { dePromisifyPatch } from "cumcord-tools";
 
 import CumZone from "../components/CumZone.jsx";
 
 export default () =>
-    patchSettingsView((SettingsView) =>
-        after("getPredicateSections", SettingsView.prototype, (_, retVal) => {
-            // don't inject into server settings!!!
-            if (retVal[1]?.section != "My Account") return;
+    dePromisifyPatch(
+        findAsync(() => findByDisplayName("SettingsView")),
+        (SettingsView) =>
+            after(
+                "getPredicateSections",
+                SettingsView.prototype,
+                (_, retVal) => {
+                    // don't inject into server settings!!!
+                    if (retVal[1]?.section != "My Account") return;
 
-            // add myself underneath cumcord! (find plugins, +1 to skip past it)
-            let index =
-                retVal.findIndex((val) => val.section == "CUMCORD_PLUGINS") + 1;
+                    // add myself underneath cumcord! (find plugins, +1 to skip past it)
+                    let index =
+                        retVal.findIndex(
+                            (val) => val.section == "CUMCORD_PLUGINS"
+                        ) + 1;
 
-            retVal.splice(index, 0, {
-                section: "ysink_zone_CUMZONE",
-                label: "The Cum Zone",
-                element: () => <CumZone />,
-            });
+                    retVal.splice(index, 0, {
+                        section: "ysink_zone_CUMZONE",
+                        label: "The Cum Zone",
+                        element: CumZone,
+                    });
 
-            return retVal;
-        })
+                    return retVal;
+                }
+            )
     );
--- cumstain/patches/settingsEntry.js
+++ cumstain/patches/settingsEntry.js
@@ -1,24 +1,26 @@
-import { findByDisplayName } from "@cumcord/modules/webpack";
+import { findByDisplayName, findAsync } from "@cumcord/modules/webpack";
 import { after } from "@cumcord/patcher";
 import SettingsMain from "../components/SettingsMain";
-import { patchSettingsView } from "cumcord-tools";
+import { dePromisifyPatch } from "cumcord-tools";
 
 export default () =>
-    patchSettingsView((SettingsView) =>
-        after("getPredicateSections", SettingsView.prototype, (_, ret) => {
-            // don't inject into server settings!!!
-            if (ret[1]?.section != "My Account") return;
+    dePromisifyPatch(
+        findAsync(() => findByDisplayName("SettingsView")),
+        (SettingsView) =>
+            after("getPredicateSections", SettingsView.prototype, (_, ret) => {
+                // don't inject into server settings!!!
+                if (ret[1]?.section != "My Account") return;
 
-            // add myself underneath cumcord! (find plugins, +1 to skip past it)
-            let index =
-                ret.findIndex((e) => e.section == "CUMCORD_PLUGINS") + 1;
+                // add myself underneath cumcord! (find plugins, +1 to skip past it)
+                let index =
+                    ret.findIndex((e) => e.section == "CUMCORD_PLUGINS") + 1;
 
-            ret.splice(index, 0, {
-                section: "ysink_stain_CUMSTAIN",
-                label: "Themes",
-                element: SettingsMain,
-            });
+                ret.splice(index, 0, {
+                    section: "ysink_stain_CUMSTAIN",
+                    label: "Themes",
+                    element: SettingsMain,
+                });
 
-            return ret;
-        })
+                return ret;
+            })
     );
