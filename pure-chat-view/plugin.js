ed03fdc89f7e30b5da4f73f685be6b32dfe7b0c9
yellowsink <yellowsink@users.noreply.github.com>
2022-02-19 23:31:50 +0000

    deploy: 8dc93da21781dce548cc1384b362af2f1f625a23

---
+++ pure-chat-view/plugin.js
@@ -0,0 +1,7 @@
+(function(s,t){"use strict";const i=t.findByProps("activityPanel").sidebar,o=t.findByProps("membersGroup").container,a=`
+nav,                  /* server list */
+section,              /* channel header */
+.${i},     /* channel sidebar */
+.${o} { /* member list */
+  display: none !important;
+}`;let e=null;function r(n){n.code==="KeyH"&&n.ctrlKey&&n.getModifierState("Alt")&&(e===null?e=s.injectCSS(a):(e(),e=null))}var c={onLoad(){document.addEventListener("keyup",r)},onUnload(){document.removeEventListener("keyup",r),e?.()}};return c})(cumcord.patcher,cumcord.modules.webpack);
