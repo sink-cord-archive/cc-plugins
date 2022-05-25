ebffc2ec6d3e21c6594a73bc005b1709de326428
Cain Atkinson <19270622+yellowsink@users.noreply.github.com>
2022-05-25 13:54:55 +0000

    recon

--- plugins/devkitplus/api.js
+++ plugins/devkitplus/api.js
@@ -2,6 +2,7 @@ import * as cctools from "cumcord-tools";
 import bestFindMethod from "./bestFindMethod";
 
 import { findAll } from "@cumcord/modules/webpack";
+import { after } from "@cumcord/patcher";
 
 const findClassNameModuleAll = (className) => {
 	if (className.startsWith(".")) className = className.slice(1);
@@ -26,12 +27,30 @@ const autoModuleFind = (selectorOrElement) =>
 		)
 	);
 
+let recons = [];
+
+const recon = (funcName, parent, oneTime = false) => {
+	const unrecon = after(
+		funcName,
+		parent,
+		(args, ret) => console.log("|| RECON: ", funcName, " ||", args, ret),
+		oneTime
+	);
+	recons.push(unrecon);
+	return unrecon;
+};
+
 window.dk = {
 	cctools,
 	bestFindMethod,
 	autoModuleFind,
 	findClassNameModuleAll,
 	findClassNameModule: (className) => findClassNameModuleAll(className)[0],
+	recon,
+	unrecon: () => {
+		recons.forEach((p) => p());
+		recons = [];
+	},
 };
 
 export default () => delete window.dk;
