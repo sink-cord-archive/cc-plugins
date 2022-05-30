4e8a13923ccb4f0ebfdfdd42804e2a7122e8f110
yellowsink <yellowsink@riseup.net>
2022-05-30 14:51:35 +0100

    [activity] fix!

--- plugins/channel-activity/WPMODULES.js
+++ plugins/channel-activity/WPMODULES.js
@@ -1,9 +1,9 @@
 import { batchFind } from "@cumcord/modules/webpack";
 
-export const [{ icon }, { children }, MemberListItem] = batchFind(
+export const [{ icon }, { children }, Clickable] = batchFind(
 	({ findByProps, findByDisplayName }) => {
 		findByProps("icon", "textRuler");
 		findByProps("nameAndDecorators", "children");
-		findByDisplayName("MemberListItem");
+		findByDisplayName("Clickable");
 	}
 );
--- plugins/channel-activity/memberListPatch.jsx
+++ plugins/channel-activity/memberListPatch.jsx
@@ -1,33 +1,27 @@
-import { findInReactTree } from "@cumcord/utils";
 import { after } from "@cumcord/patcher";
-import { MemberListItem } from "./WPMODULES";
+import { Clickable } from "./WPMODULES";
+import { findInReactTree } from "@cumcord/utils";
 
-export default after("render", MemberListItem.prototype, (_, ret) => {
-	// get activities
+export default after("renderInner", Clickable.prototype, (__, ret) => {
+	if (!ret.props["data-list-item-id"]?.startsWith("members-")) return;
 	const activities = findInReactTree(
-		ret?.props?.subText,
-		(m) => m?.activities?.length
+		ret,
+		(n) => n.activities?.length
 	)?.activities;
 	if (!activities) return;
 
-	// remove exising children
-	ret.props.children = [];
+	const target = ret.props.children;
 
-	// add a child for each activity
 	for (const activity of activities) {
 		const activityImage =
 			activity?.assets?.large_image ?? activity?.assets?.small_image;
-
 		if (!activity.application_id || !activityImage) continue;
-
 		const activityImgUrl = activityImage.startsWith("mp:")
 			? activityImage.replace("mp:", "https://media.discordapp.net/")
 			: `https://cdn.discordapp.com/app-assets/${activity.application_id}/${activityImage}.png`;
 
-		ret.props.children.push(
+		target.props.children.push(
 			<img src={activityImgUrl} className="ysink_activity_image" />
 		);
 	}
-
-	return ret;
 });
