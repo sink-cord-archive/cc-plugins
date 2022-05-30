47ded8641aa82a623d85af14793159f212a62958
yellowsink <yellowsink@riseup.net>
2022-05-30 10:18:13 +0100

    [deaf] tune the regex :trol:

--- plugins/tonedeaf/index.js
+++ plugins/tonedeaf/index.js
@@ -3,5 +3,5 @@ import { FluxDispatcher } from "@cumcord/modules/common";
 
 export const onUnload = before("dispatch", FluxDispatcher, ([arg]) => {
 	if (arg.type === "MESSAGE_CREATE")
-		arg.message.content = arg.message.content.replaceAll(/\/.*?(?: |$)/g, "");
+		arg.message.content = arg.message.content.replaceAll(/ \/.*?(?: |$)/g, "");
 });
