5b700e8b10bdb0b7c22351fed7ce60be19839d36
yellowsink <yellowsink@riseup.net>
2022-06-06 18:14:39 +0100

    [dk] add shotgunrecon

--- plugins/devkitplus/api.js
+++ plugins/devkitplus/api.js
@@ -1,7 +1,7 @@
 import * as cctools from "cumcord-tools";
 import bestFindMethod from "./bestFindMethod";
 
-import { findAll } from "@cumcord/modules/webpack";
+import { findAll, findByKeywordAll, modules } from "@cumcord/modules/webpack";
 import { after, injectCSS } from "@cumcord/patcher";
 import grass from "./grass";
 
@@ -34,12 +34,41 @@ const recon = (funcName, parent, oneTime = false) => {
 	const unrecon = after(
 		funcName,
 		parent,
-		(args, ret) => console.log("|| RECON: ", funcName, " ||", args, ret),
+		(args, ret) =>
+			console.log(
+				"%cRECON: " + funcName,
+				"background-color: #9d65ff; color: white; border-radius: 4px; padding: 0px 6px 0px 6px;",
+				"",
+				args,
+				ret
+			),
 		oneTime
 	);
 	recons.push(unrecon);
 	return unrecon;
 };
+
+const shotgunRecon = (parentOrKeyword, oneTime = false) => {
+	const reconObj = (obj) => {
+		for (const k in obj)
+			if (typeof obj[k] === "function")
+				try {
+					recon(k, obj, oneTime);
+				} catch {}
+	};
+
+	if (typeof parentOrKeyword === "string")
+		findByKeywordAll(parentOrKeyword).forEach(reconObj);
+	else reconObj(parentOrKeyword);
+};
+
+// this is irresponsible
+/*const atomBombRecon = () => {
+	for (const module of Object.values(modules))
+		try {
+			shotgunRecon(module.exports);
+		} catch {}
+};*/
 
 const injectSCSS = (scss, pretty = false) => {
 	const ret = injectCSS(grass(scss, pretty));
@@ -53,7 +82,11 @@ window.dk = {
 	autoModuleFind,
 	findClassNameModuleAll,
 	findClassNameModule: (className) => findClassNameModuleAll(className)[0],
-	recon,
+	recon: {
+		snipe: recon,
+		shotgun: shotgunRecon,
+		//atomBomb: atomBombRecon,
+	},
 	unrecon: () => {
 		recons.forEach((p) => p());
 		recons = [];
