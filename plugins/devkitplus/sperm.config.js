5a8592a450df76f6ec92121ff23c95b621e32440
yellowsink <yellowsink@riseup.net>
2022-07-10 11:38:57 +0100

    dont format grass

--- plugins/devkitplus/sperm.config.js
+++ plugins/devkitplus/sperm.config.js
@@ -2,8 +2,10 @@ const arraybuffer = require("@wemap/rollup-plugin-arraybuffer");
 
 module.exports = {
 	rollup: {
-		inPlugins: [arraybuffer({
-			include: "**/*.wasm"
-		})],
-	}
+		inPlugins: [
+			arraybuffer({
+				include: "**/*.wasm",
+			}),
+		],
+	},
 };
