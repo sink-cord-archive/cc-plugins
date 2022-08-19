f443db7c0cd26f3a91e0d47f23ffad6e37de7667
redstonekasi <kasi@hanneskann.de>
2022-08-19 21:30:22 +0200

    [usrbg] fix banner styles and overall improvement

--- plugins/usrbg/index.ts
+++ plugins/usrbg/index.ts
@@ -1,20 +1,19 @@
-import fixUserObjects from "./patches/fixUserObjects";
-import patchBannerStyles from "./patches/patchBannerStyles";
 import getDb from "./usrbg-db";
+import patchBanner from "./patches/patchBanner";
 
 // prevents patches from applying if async resolves post-unload
 let cancelPatches = false;
 
-let patches: (() => void)[] = [];
+let unpatchBanner;
 
 export async function onLoad() {
 	const db_cache = await getDb();
 
 	if (!cancelPatches)
-		patches = [patchBannerStyles(db_cache), fixUserObjects(db_cache)];
+		unpatchBanner = patchBanner(db_cache);
 }
 
 export function onUnload() {
 	cancelPatches = true;
-	window._.forEachRight(patches, (p) => p());
+	unpatchBanner?.();
 }
